0a598c0f659da3da8267ad3e36c25b04c19847f3
tylerhartley <tyleha@gmail.com>
2012-11-21 12:22:29 -0800

    computing imports

---
+++ computing_imports.py
@@ -0,0 +1,13 @@
+import sys, os
+sys.path.append('C:\\Users\\Tyler\\.ipython\\Simscore-Computing')
+
+import numpy as np
+import time, datetime
+import json
+import getopt, string
+import cStringIO, base64
+import boto
+import boto.sqs.message
+import ast
+import pickle
+import math

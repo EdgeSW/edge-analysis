537c6f40f8fc22ee4d1e857369b9560ebc694aea
Tyler Hartley <thartley@simulab.com>
2013-02-07 23:38:54 +0000

    new known error detected. Need to update knownerrors.py to allow multiple known errors per tool

--- report/KnownErrors.py
+++ report/KnownErrors.py
@@ -1,5 +1,6 @@
 knownerrors = {
             '0100001E':['Fg_R', 'OutOfRange', 'min', -10.0]
             ,'40000011':['ThG_L', 'OutOfRange', 'min', -20.0]
+	     ,'4100002F':['Fg_L', 'OutOfRange', 'min', -10.0]
             }
 
